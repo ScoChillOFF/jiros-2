c4154608889a736a9010077e1c670becb4d4e299
ScoChillOFF <cheatkiller12@gmail.com>
2025-08-19 14:13:21 +0300

    make project creation,  stylized empty-list message

--- src/app/pages/projects/projects-page.component.ts
+++ src/app/pages/projects/projects-page.component.ts
@@ -1,35 +1,63 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { ProjectService } from '../../services/project/project.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { of } from 'rxjs';
+import { TuiButton, TuiDialogService, TuiTextfield } from '@taiga-ui/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-projects-page.component',
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TuiButton,
+    ReactiveFormsModule,
+    TuiTextfield,
+  ],
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.less',
 })
 export class ProjectsPageComponent {
   private projectService = inject(ProjectService);
+  private readonly dialogs = inject(TuiDialogService);
 
-  // readonly projects$ = this.projectService.myProjects$;
-  projects$ = of([
-    {
-      id: '1',
-      name: 'Project 1',
-    },
-    {
-      id: '2',
-      name: 'Project 2',
-    },
-    {
-      id: '3',
-      name: 'Project 3',
-    },
-    {
-      id: '4',
-      name: 'Project 4',
-    },
-  ]);
+  @ViewChild('createDialog', { static: true })
+  private createDialogTpl!: TemplateRef<unknown>;
+
+  form = new FormGroup({
+    projectName: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  readonly projects$ = this.projectService.myProjects$;
+
+  openCreate(): void {
+    this.dialogs
+      .open<string>(this.createDialogTpl, {
+        label: 'Create project',
+        size: 'm',
+        dismissible: true,
+      })
+      .subscribe((name) => {
+        if (name) {
+          this.projectService.createProject$(name).subscribe();
+          console.log('Create project:', name);
+        }
+      });
+  }
+
+  submit(observer: { next: (v: string) => void; complete: () => void }): void {
+    if (this.form.valid) {
+      observer.next(this.form.value.projectName);
+      observer.complete();
+      this.form.reset();
+    }
+  }
 }
