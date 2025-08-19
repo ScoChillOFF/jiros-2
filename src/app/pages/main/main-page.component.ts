b61650ed755139bcda20f5541f2c18daf942229b
ScoChillOFF <cheatkiller12@gmail.com>
2025-08-19 11:24:48 +0300

    make static sidebar

--- src/app/pages/main/main-page.component.ts
+++ src/app/pages/main/main-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-main-page.component',
-  imports: [],
+  imports: [SidebarComponent, RouterModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.less',
 })
