c5b4cc33ba3a4ed011bfa5143da4358ba0396092
ScoChillOFF <cheatkiller12@gmail.com>
2025-08-19 13:17:06 +0300

    make static projects list

--- src/app/pages/projects/projects-page.component.ts
+++ src/app/pages/projects/projects-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProjectService } from '../../services/project/project.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-projects-page.component',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.less',
 })
-export class ProjectsPageComponent {}
+export class ProjectsPageComponent {
+  private projectService = inject(ProjectService);
+
+  // readonly projects$ = this.projectService.myProjects$;
+  projects$ = of([
+    {
+      id: '1',
+      name: 'Project 1',
+    },
+    {
+      id: '2',
+      name: 'Project 2',
+    },
+    {
+      id: '3',
+      name: 'Project 3',
+    },
+    {
+      id: '4',
+      name: 'Project 4',
+    },
+  ]);
+}
