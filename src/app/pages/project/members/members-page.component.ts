34d625e5a6a06bc1a009e72423dd92c8e66f3a59
ScoChillOFF <cheatkiller12@gmail.com>
2025-08-20 08:01:45 +0300

    make adding and removing members from project

--- src/app/pages/project/members/members-page.component.ts
+++ src/app/pages/project/members/members-page.component.ts
@@ -1,50 +1,44 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { UserService } from '../../../services/users/users.service';
 import { MemberService } from '../../../services/members/members.service';
 import { CommonModule } from '@angular/common';
-import { of, switchMap } from 'rxjs';
+import { switchMap } from 'rxjs';
 import { UserCardComponent } from '../../../components/user-card/user-card.component';
 import { SearchBoxComponent } from '../../../components/search-box/search-box.component';
 import { User } from '../../../models/user.interface';
+import { TuiButtonClose } from '@taiga-ui/kit';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-members-page.component',
-  imports: [CommonModule, UserCardComponent, SearchBoxComponent],
+  imports: [
+    CommonModule,
+    UserCardComponent,
+    SearchBoxComponent,
+    TuiButtonClose,
+    TuiButton,
+  ],
   templateUrl: './members-page.component.html',
   styleUrl: './members-page.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MembersPageComponent {
   private userService = inject(UserService);
   private memberService = inject(MemberService);
 
   searchFn$ = this.userService.searchUsersByEmailPrefix$.bind(this.userService);
+
   readonly isOwner$ = this.userService.currentUser$.pipe(
     switchMap((u) => this.memberService.isOwner$(u?.id ?? null))
   );
 
-  // readonly members$ = this.memberService.members$;
-  members$ = of([
-    {
-      id: '1',
-      email: 'example@mail.com',
-      displayName: null,
-      photoURL: '',
-      projects: [],
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-    {
-      id: '2',
-      email: 'anotherexample@mail.com',
-      displayName: 'John Doe',
-      photoURL: '',
-      projects: [],
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-  ]);
+  readonly members$ = this.memberService.members$;
+
+  onRemoveMember(memberId: string) {
+    this.memberService.removeMember$(memberId).subscribe();
+  }
 
   onPick(user: User) {
-    console.log('User selected from search:', user);
+    this.memberService.addMember$(user.id).subscribe();
   }
 }
