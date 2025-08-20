0dbf8f0e7e11ad52b41b1ce4598959278a84a99f
ScoChillOFF <cheatkiller12@gmail.com>
2025-08-20 03:36:31 +0300

    fix layout, change bg-color, add scroll on children pages

--- src/app/pages/project/project-page.component.ts
+++ src/app/pages/project/project-page.component.ts
@@ -1,12 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { ProjectService } from '../../services/project/project.service';
 import { CommonModule } from '@angular/common';
-import { TuiTabsHorizontal, TuiTab } from "@taiga-ui/kit";
+import { TuiTabsHorizontal, TuiTab } from '@taiga-ui/kit';
 import { RouterModule } from '@angular/router';
+import { TuiScrollbar } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-project-page.component',
-  imports: [CommonModule, TuiTabsHorizontal, TuiTab, RouterModule],
+  imports: [
+    CommonModule,
+    TuiTabsHorizontal,
+    TuiTab,
+    RouterModule,
+    TuiScrollbar,
+  ],
   templateUrl: './project-page.component.html',
   styleUrl: './project-page.component.less',
 })
